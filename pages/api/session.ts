0c3a4f3c46eb7690707794414b6e65e22765e771
ahmadchata <ahmadchata@gmail.com>
2023-05-06 01:52:04 +0100

    Fix headers bug

--- pages/api/session.ts
+++ pages/api/session.ts
@@ -6,7 +6,7 @@ const VALID_PASSWORD = "password";
 
 export default withSessionRoute(createSessionRoute);
 
-async function createSessionRoute(req: any, res: any) {
+async function createSessionRoute(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "POST") {
     const { email, password } = req.body;
 
@@ -18,7 +18,5 @@ async function createSessionRoute(req: any, res: any) {
       await req.session.save();
       res.send({ ok: true });
     }
-    return res.status(403).send("");
   }
-  return res.status(404).send("");
 }
