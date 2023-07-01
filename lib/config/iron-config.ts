d494cea8b289778b80b69b57e853448906be135f
ahmadchata <ahmadchata@gmail.com>
2023-07-01 21:14:19 +0100

    Set cookie to expire after 2 days

--- lib/config/iron-config.ts
+++ lib/config/iron-config.ts
@@ -2,6 +2,7 @@ export const sessionOptions = {
   password: "zQo4T8apfbdwctV1Cso1oAndQT9qwC95",
   cookieName: "APP_COOKIE",
   cookieOptions: {
+    maxAge: 60 * 60 * 24 * 2, // 2 days
     secure: process.env.NODE_ENV === "production" ? true : false,
   },
 };
